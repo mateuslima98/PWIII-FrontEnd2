49f610a28a8135ce10749ecb30b071ea9d1354eb
Mateus98 <mateus_lima98@hotmail.com>
2019-11-29 21:30:49 -0300

    Exercicio23

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { componentFactoryName } from '@angular/compiler';
+import { ListaOcorrenciaComponent } from './lista-ocorrencia/lista-ocorrencia.component';
+import { NovoOcorrenciaComponent } from './novo-ocorrencia/novo-ocorrencia.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path: 'listagem', component: ListaOcorrenciaComponent },
+{ path: 'novo', component:NovoOcorrenciaComponent}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
